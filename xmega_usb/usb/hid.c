4e08091436481fc5eab3888a15927fde9d7cd441
kuro68k <mojo@world3.net>
2026-09-02 13:31:15 +0100

    Additional HID functionality

--- xmega_usb/usb/hid.c
+++ xmega_usb/usb/hid.c
@@ -6,16 +6,55 @@
  */
 
 #include <avr/io.h>
+#include <avr/sleep.h>
 #include "usb.h"
 #include "usb_config.h"
 
 uint8_t hid_report[USB_HID_REPORT_SIZE] __attribute__((__aligned__(2)));
+uint8_t out_hid_report[USB_HID_OUT_REPORT_SIZE] __attribute__((__aligned__(2)));
+volatile bool out_hid_report_received_SIG = 0;
 
-
-/* Send HID reports. Blocks until the endpoint is ready.
- */
+/****************************************************************************************
+* Send HID report to host. Blocks until endpoint is ready or USB is suspended.
+*/
 void hid_send_report(void)
 {
-	while (!usb_ep_is_ready(0x81));
+	while (!usb_ep_is_ready(0x81))
+	{
+		if (usb_suspended_AT)
+			return;
+		sleep_cpu();
+	}
 	usb_ep_start_in(0x81, hid_report, USB_HID_REPORT_SIZE, false);
+}
+
+/****************************************************************************************
+* Wait for HID endpoint to become read.
+* Blocks until endpoint is ready or USB is suspended.
+*/
+void hid_wait_until_ready(void)
+{
+	while (!usb_ep_is_ready(0x81))
+	{
+		if (usb_suspended_AT)
+			return;
+		sleep_cpu();
+	}
+}
+
+/****************************************************************************************
+* Check if HID endpoint is ready.
+*/
+bool hid_is_ready(void)
+{
+	return usb_ep_is_ready(0x81);
+}
+
+/****************************************************************************************
+* Call to enable reception of HID OUT reports.
+*/
+void hid_get_report(void)
+{
+	out_hid_report_received_SIG = 0;
+	usb_ep_start_out(0x01, out_hid_report, USB_HID_OUT_REPORT_SIZE);
 }
--- xmega_usb/usb/hid.h
+++ xmega_usb/usb/hid.h
@@ -8,11 +8,22 @@
 #ifndef HID_H_
 #define HID_H_
 
+#include <stdbool.h>
+
 
 extern uint8_t hid_report[USB_HID_REPORT_SIZE];
-
+extern uint8_t out_hid_report[USB_HID_OUT_REPORT_SIZE];
+extern volatile bool out_hid_report_received_SIG;
 
 extern void hid_send_report(void);
+extern void hid_wait_until_ready(void);
+extern bool hid_is_ready(void);
+extern void hid_get_report(void);
+
+inline static void hid_get_report_received_callback(void)
+{
+	out_hid_report_received_SIG = 0xFF;
+}
 
 
 #endif /* HID_H_ */
